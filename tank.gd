2304df7f513341026aed20c3baadef9b4fc1c5a5
Andre Starosta <a.starosta98@gmail.com>
2021-03-06 21:52:24 -0300

    add demo for tank movement/aiming

---
+++ tank.gd
@@ -0,0 +1,56 @@
+extends KinematicBody2D
+
+onready var tank_sprite:AnimatedSprite = $tank
+onready var cannon:Sprite = $cannon
+onready var charge_bar:TextureProgress = $cannon/chargeBar
+
+var shot_charge = 0
+var shot_increasing = true
+
+const move_speed = 100
+
+func _physics_process(delta):
+    process_movement(delta)
+    process_aim()
+    process_fire()
+
+func process_movement(delta):
+    var direction = Vector2(0, 0)
+    if Input.is_action_pressed("ui_up"):
+        direction.y = -1
+    if Input.is_action_pressed("ui_down"):
+        direction.y = 1
+    if Input.is_action_pressed("ui_left"):
+        direction.x = -1
+    if Input.is_action_pressed("ui_right"):
+        direction.x = 1
+    tank_sprite.animation = resolve_sprite_direction(direction)
+    
+    move_and_slide(direction * move_speed) 
+    
+func resolve_sprite_direction(direction):
+    if direction.y == 0 and direction.x != 0:
+        return "horizontal"
+    if direction.x == 0 and direction.y != 0:
+        return "vertical"
+    if direction.x == 1 and direction.y == 1 or direction.x == -1 and direction.y == -1:
+        return "main_diagonal"
+    if direction.x == -1 and direction.y == 1 or direction.x == 1 and direction.y == -1:
+        return "anti_diagonal"
+    return tank_sprite.animation
+    
+func process_aim():
+    cannon.look_at(get_global_mouse_position())
+
+func process_fire():
+    if Input.is_mouse_button_pressed(BUTTON_LEFT):
+        if(shot_increasing):
+            shot_charge += 1
+            shot_increasing = shot_charge <= 100
+        else:
+            shot_charge -= 1
+            shot_increasing = shot_charge <= 0
+    else:
+        shot_charge = 0
+        
+    charge_bar.value = shot_charge
