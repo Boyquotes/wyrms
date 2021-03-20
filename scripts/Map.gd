738e918489b7dc885834d1cef1bdf9a6a868a8d5
Andre Starosta <a.starosta98@gmail.com>
2021-03-20 16:13:49 -0300

    add adaptive walls

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -1,16 +1,26 @@
 extends Node2D
 
+onready var tilemap:TileMap = $FGTilemap
+onready var cell_nodes:Node2D = $cellNodes
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var wall_scene:PackedScene = load("res://scenes/tiles/wall.tscn")
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    populate_tilemap_with_logic_nodes()
 
+func populate_tilemap_with_logic_nodes():
+    var cells = tilemap.get_used_cells_by_id(4)
+    for cell in cells:
+        var cell_node = wall_scene.instance()
+        cell_node.coordinate = cell
+        cell_node.tilemap = tilemap
+        print(tilemap.cell_size)
+        cell_node.position = Vector2(cell.x * tilemap.cell_size.x, cell.y * tilemap.cell_size.y)
+        cell_nodes.add_child(cell_node)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func get_spawn_points():
+    var children = $spawnPoints.get_children()
+    var points = []
+    for child in children:
+        points.append(child.position)
+    return points
--- scripts/wall.gd
+++ scripts/wall.gd
@@ -6,11 +6,15 @@ var tilemap:TileMap
 var max_health = 30
 var health = max_health
 
+func _process(delta):
+    if tilemap.get_cellv(coordinate + Vector2(0, 1)) == -1:
+        tilemap.set_cellv(coordinate, 3)
+
 func explode(damage):
     health -= damage
 
     if(health <= 0):
-        tilemap.set_cellv(coordinate, 1)
+        tilemap.set_cellv(coordinate, -1)
         queue_free()
     print("wall" + str(health/max_health))
     $crack.modulate.a = 1 - health/max_health
